3b491249aef0d5d8832a1846e54ddfeee63611ba
Musluhan Çavuş <91620498+Sivellexfc@users.noreply.github.com>
2022-05-05 22:39:17 +0300

    Add files via upload

---
+++ Patika.dev/Odev - 3 ve 4'e Tam Bolunen Sayilar/BolunenSayilariBulma/src/App.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class App {
+
+    /*
+    
+    Java döngüler ile kullanıcının girdiği sayıya kadar çift olan sayıları bulan programı yazıyoruz.
+
+    Ödev
+    Java döngüler ile 0'dan girilen sayıya kadar olan sayılardan 3 ve 4'e tam bölünen sayıların ortalamasını hesaplayan programı yazınız. 
+    
+    */
+
+    public static void main(String[] args) throws Exception {
+
+        int sum = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Deger giriniz   : ");
+        int number = scanner.nextInt();
+
+        scanner.close();
+        
+        for (int i = 0; i < number; i++) {
+
+            if(i%3 == 0 && i%4 == 0) {
+
+                sum = sum + i;
+            }
+            
+        }
+
+        System.out.print(sum);
+
+    }
+}
