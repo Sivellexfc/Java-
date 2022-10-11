bd284648d3ea1cb210fee7689961a62dd395e5b0
Musluhan Çavuş <91620498+Sivellexfc@users.noreply.github.com>
2022-10-11 10:12:53 +0300

    Add files via upload

---
+++ Mid Level Java Codes/LinkedList/src/LabDoublyLinkedList.java
@@ -0,0 +1,58 @@
+public class LabDoublyLinkedList<T> extends DoublyLinkedList<T> {
+    /**
+     * Çift bağlı listenin istenilen konumuna eleman ekler
+     * @param index konum
+     * @param value eklenecek eleman
+     */
+    @Override
+    public void insertAt(int index, T value) {
+
+        if(index == 0){
+            addFirst(value);
+        }
+        else if (index == size()) {
+            addLast(value);
+        }
+        else {
+            int i =0;
+            Node<T> node = getHead();
+            while(node != null && i<index){
+                node = node.next;
+                i++;
+            }
+            Node<T> newNode = new Node<T>(value,node,node.previous);
+            node.previous.next = newNode;
+            node.previous = newNode;
+            setSize(size() + 1);
+        }
+    }
+
+    /**
+     * Verilen konumdaki elemanı siler
+     * @param index silinecek elemanın konumu
+     * @return silinen değer
+     */
+    @Override
+    public T removeAt(int index) {
+
+        if(index == 0){
+            return removeFirst();
+        }
+        else if (index == size()-1) {
+            return removeLast();
+        }
+        else {
+            int i =0;
+            Node<T> node = getHead();
+            while(node != null && i<index){
+                node = node.next;
+                i++;
+            }
+            node.previous.next = node.next;
+            node.next.previous = node.previous;
+            setSize(size() - 1);
+            return node.value;
+        }
+
+    }
+}
